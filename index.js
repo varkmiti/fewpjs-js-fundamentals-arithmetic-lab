9aca0baeae722136129b488b70e4a1fb9e0a95e8
Mark Viti <+>
2022-07-10 19:54:46 -0400

    commit tree 903c66fb8c069094f8131f51b118f902517635db

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2; 
 
 var multiply = num1 * num2;
 
-var random = Math.random();
+var random = Math.floor(Math.random()*10);
+
+const num3 = 36;
+const num4 = 8; 
 
 var mod = num3 % num4; 
 
